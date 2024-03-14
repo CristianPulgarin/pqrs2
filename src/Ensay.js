97896c0a98ba562b541fec86d6cdc631f8dceb9c
cristian <cristianpulga13@gmail.com>
2024-03-14 15:50:47 -0500

    correcto

--- src/Ensay.js
+++ src/Ensay.js
@@ -1,41 +1,52 @@
-import React from 'react'
+import React , {useRef} from 'react'
 import './txt.css'
 const Ensay = () => {
-    return (
-        <div className='text'>
-          <p style={{ fontFamily: 'Times New Roman', fontSize: '20px', lineHeight: '1.5' , textAlign:'justify'}}>
-          Medellín, 14 de marzo de 2024 <br></br>
-          <br></br>
-Señor(a)<br></br><br></br>
 
-Titular de los datos <br></br>
-<br></br>
-Referencia: Radicado  <br></br>
-Respuesta: Revocatoria de la autorización para tratamiento de datos<br></br><br></br>
-Cordial saludo, Conforme el reclamo efectuado por usted el pasado,
-y encontrándonos dentro del término estipulado <br></br>
-en la Ley 1581 de 2012 para la atenciónde reclamos, nos permitimos otorgarle una respuesta de la siguiente manera:<br></br>
-<br></br>
-De acuerdo con la solicitud de revocar la autorización para que la Compañía trate los datos personales<br></br>
-de ,nos permitimos comunicar que, en cumplimiento de nuestros deberes como Responsables del<br></br>
-Tratamiento de los datos personales, y el compromiso adquirido al momento de obtener la autorización,<br></br>
-la Compañía se abstendrá de continuar ejecutando las finalidades para las cuales había capturado los datos <br></br>
-personales, salvo en aquellos casos en que exista un deber legal o
-contractual que requieran la permanencia<br></br>
- de los datos personales en la base de datos allí.
- <br></br><br></br>
-De esta manera damos respuesta a su consulta dentro de los términos del artículo 15 de la Ley 1581 de 2012.<br></br>
-<br/>Cabe mencionar que, la Compañía podrá garantizar la revocatoria de sus datos pasadas las 24 horas<br></br>
-siguientes a partir del momento que reciba esta comunicación.
-<br></br><br></br>
+    const textRef = useRef(null);
+
+  const copyToClipboard = () => {
+    const range = document.createRange();
+    range.selectNode(textRef.current);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+    alert('Texto copiado al portapapeles');
+  };
+
+    const texto = `Medellín, 14 de marzo de 2024
+
+Señor(a)
+
+Titular de los datos
+
+Referencia: Radicado
+Respuesta: Revocatoria de la autorización para tratamiento de datos
+
+Cordial saludo, Conforme el reclamo efectuado por usted el pasado ,
+y encontrándonos dentro del término estipulado en la Ley 1581 de 2012 para la atención
+de reclamos, nos permitimos otorgarle una respuesta de la siguiente manera:
+
+De acuerdo con la solicitud de revocar la autorización para que la Compañía trate los
+datos personales de ,nos permitimos comunicar que,
+en cumplimiento de nuestros deberes como Responsables del Tratamiento de los datos
+personales, y el compromiso adquirido al momento de obtener la autorización,
+la Compañía se abstendrá de continuar ejecutando las finalidades para las cuales había
+capturado los datos personales, salvo en aquellos casos en que exista un deber legal o
+contractual que requieran la permanencia de los datos personales en la base de datos allí.
+
+De esta manera damos respuesta a su consulta dentro de los términos del artículo 15 de la Ley 1581 de 2012.
+Cabe mencionar que, la Compañía podrá garantizar la revocatoria de sus datos pasadas las 24 horas siguientes
+a partir del momento que reciba esta comunicación.
+
 Cordialmente,
-<br></br><br></br>
 
-<br></br><br></br>
-Oficial de Cumplimiento de Datos Personales
+Oficial de Cumplimiento de Datos Personales`;
 
-          </p>
-        </div>)
+  return (
+    <div className='text'>
+       <textarea rows="10" cols="50" defaultValue={texto} readOnly />
+    </div>)
 }
 
 export {Ensay}
