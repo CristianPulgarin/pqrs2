2f1eb300d92b8e2ef478d6148e0995ff084b1e90
cristian <cristianpulga13@gmail.com>
2024-03-15 15:11:15 -0500

    boton

--- src/FormRev/TextRev.js
+++ src/FormRev/TextRev.js
@@ -20,25 +20,25 @@ const TextRev = (props) => {
     
     <div className="conteRM" >
    <p className="textRM" ref={textRef}> 
-   Medellín, {props.day} de {props.mes} de {props.year}
-   <br/><br/>
-          Señor(a) {props.nombre}
+   Medellín, {props.day} de {props.mes} de {props.year}<br/>
+   <br/>
+          Señor(a) {props.nombre}<br/>
           <br/>
-          Titular de los datos
+          Titular de los datos<br/>
           <br/>
           Referencia: Radicado {props.caso}<br/>
           Respuesta: Revocatoria de la autorización para tratamiento de datos<br/>
           <br/>
           Cordial saludo,
-          <br/>
-          Conforme el reclamo efectuado por usted el pasado ,<br/>
+          <br/><br/>
+          Conforme el reclamo efectuado por usted el pasado {props.fecha},<br/>
           y encontrándonos dentro del término estipulado en la Ley 1581 de 2012 para la atención<br/>
           de reclamos, nos permitimos otorgarle una respuesta de la siguiente manera:<br/>
           <br/>
           De acuerdo con la solicitud de revocar la autorización para que la Compañía trate los<br/>
-          datos personales de {props.nombre}, nos permitimos comunicar que,<br/>
-          en cumplimiento de nuestros deberes como Responsables del Tratamiento de los datos<br/>
-          personales, y el compromiso adquirido al momento de obtener la autorización,<br/>
+          datos personales de {props.nombre}, nos permitimos comunicar que, en cumplimiento de nuestros deberes <br/>
+          como Responsables del Tratamiento de los datos personales, y el compromiso adquirido<br/>
+           al momento de obtener la autorización,<br/>
           la Compañía se abstendrá de continuar ejecutando las finalidades para las cuales había<br/>
           capturado los datos personales, salvo en aquellos casos en que exista un deber legal o<br/>
           contractual que requieran la permanencia de los datos personales en la base de datos allí.<br/>
@@ -48,13 +48,14 @@ const TextRev = (props) => {
           a partir del momento que reciba esta comunicación.<br/>
           <br/>
           Cordialmente,<br/>
+          {props.marca}
           <br/>
           Oficial de Cumplimiento de Datos Personales
 </p>
 <button className="btncopy" onClick={handleCopy}>Copiar texto</button>
       </div>
       
-    
+    <br/>
     </>
   );
 };
